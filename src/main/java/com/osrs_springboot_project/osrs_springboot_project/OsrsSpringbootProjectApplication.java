03e4eafd521e70214af0f0a62e9a14734ef5e0d4
AlexFromPitt <mitro.alex412@gmail.com>
2025-01-31 13:43:46 -0500

    Initialized Project Files
    Created the SpringBoot Project

---
+++ src/main/java/com/osrs_springboot_project/osrs_springboot_project/OsrsSpringbootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.osrs_springboot_project.osrs_springboot_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsrsSpringbootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsrsSpringbootProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/osrs_springboot_project/osrs_springboot_project/OsrsSpringbootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osrs_springboot_project.osrs_springboot_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsrsSpringbootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
